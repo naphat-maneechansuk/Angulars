74014313d31ed6b1f4f2e1281461e0fdca7ab371
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-11 17:41:11 +0700

    start_component

--- LearnningAngular/src/app/app.ts
+++ LearnningAngular/src/app/app.ts
@@ -1,30 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { Component } from '@angular/core';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-   userName ="admin";
-   isLogin = true;
-   toggle(){
-      this.isLogin = !this.isLogin;
-   }
-//
-   userPermission = "admin";
-//
-   data =[
-      {name: "John", age: 30, city: "New York"},
-      {name: "Jane", age: 25, city: "Los Angeles"},
-      {name: "Mike", age: 35, city: "Chicago"}
-   ]
-   clearData(){
-      this.data = [];
-   }
 
 }
 
