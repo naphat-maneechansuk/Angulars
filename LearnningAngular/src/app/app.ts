ae05ca6d7b17c2e3395be1aa512f2a103bbdd940
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-11 14:41:55 +0700

    click on Even

--- LearnningAngular/src/app/app.ts
+++ LearnningAngular/src/app/app.ts
@@ -7,10 +7,9 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  imageurl = "https://picsum.photos/id/10/2500/1667"
-  size = 100
-  doubleSize(){
-    return this.size * 2;
+  name ="Naphat"
+  showMessege(){
+    alert("Hello " + this.name + "!");
   }
 }
 
