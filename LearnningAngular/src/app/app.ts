5d22124a1c4a1970b9c8938e3f9833c3e6f0cb3f
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-11 16:04:28 +0700

    ngClass,ngStyle

--- LearnningAngular/src/app/app.ts
+++ LearnningAngular/src/app/app.ts
@@ -1,14 +1,36 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgClass,NgStyle } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [NgClass, FormsModule, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
    userName ="admin";
-   
+   isLogin = true;
+   toggle(){
+      this.isLogin = !this.isLogin;
+   }
+//
+   userPermission = "admin";
+//
+   data =[
+      {name: "John", age: 30, city: "New York"},
+      {name: "Jane", age: 25, city: "Los Angeles"},
+      {name: "Mike", age: 35, city: "Chicago"}
+   ]
+   clearData(){
+      this.data = [];
+   }
+   //
+    isActive = false;
+    toggleActive() {
+        this.isActive = !this.isActive;
+    }
+    //pipe
 }
 
 
